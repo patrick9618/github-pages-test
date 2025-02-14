561b50ee2b9707522d61072d0edb3b68fb1d9228
patrick9618 <38888010+patrick9618@users.noreply.github.com>
2025-02-14 19:09:49 +0100

    Updated

---
+++ js/faq.js
@@ -0,0 +1,14 @@
+﻿window.faqUtils = {
+    toggleHeight: function (element, isVisible) {
+        if (!element) return;
+
+        if (isVisible) {
+            // Measure the actual height of the content
+            const scrollHeight = element.scrollHeight;
+            element.style.maxHeight = scrollHeight + "px";
+        } else {
+            // Reset max-height to collapse the element
+            element.style.maxHeight = "0px";
+        }
+    }
+};
